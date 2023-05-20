562fb6c6cbdae16b0fcd8ce6a10ad1928444005d
rafihililmif <rafihilmifirdaus@gmail.com>
2023-05-21 06:19:53 +0700

    testing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 const express = require("express");
 
-// const accounts = require("./routes/accounts");
-// const membership = require("./routes/memberships");
-
 const User = require("./models/User");
 const Products = require("./models/Products");
 const Category = require("./models/Category");
@@ -13,20 +10,11 @@ const History = require("./models/History");
 const Payment = require("./models/Payment");
 const Shipping = require("./models/Shipping");
 
-// Account.associate({Membership, Content, Saldo});
-// Membership.associate({Account, Content, Patron});
-// Content.associate({Account, Membership});
-// Saldo.associate({ Account });
-// Patron.associate({ Membership });
-
 const app = express();
 
 app.set("port", 3000);
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
-// app.use("/api", accounts);
-// app.use("/api", membership);
 
 app.listen(app.get("port"), () => {
     console.log(`Server started at http://localhost:${app.get("port")}`);
