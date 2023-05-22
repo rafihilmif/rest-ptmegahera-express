b4abb0ca82a7d39e3c72fa592f03ca8007767d12
rafihililmif <rafihilmifirdaus@gmail.com>
2023-05-22 23:09:34 +0700

    Update user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -23,10 +23,10 @@ const checkEmail = async (email) => {
             throw new Error("email is not unique")
     }
 };
-
+//REGISTER CUSTOMER
 router.post('/add/account/customer', async function (req, res) {
     let { email, password, firstName, lastName, birthdate, address, city, province, phone } = req.body;
-    const schema = Joi.object({
+        const schema = Joi.object({
         email: Joi.string().external(checkEmail).email({ minDomainSegments: 2, tlds: { allow: ['com'] } }).required(),
         password: Joi.string().required(),
         firstName: Joi.string().required(),
@@ -37,7 +37,6 @@ router.post('/add/account/customer', async function (req, res) {
         province: Joi.string().required(),
         phone: Joi.string().required()
         });
-
         try {
              await schema.validateAsync(req.body)
         }     catch (error) {
@@ -56,7 +55,6 @@ router.post('/add/account/customer', async function (req, res) {
             );
     let newIdCustomer = newIdPrefix + (similarUID.length + 1).toString().padStart(3, '0');
     const passwordHash = bcrypt.hashSync(password, 10);
-    console.log(passwordHash);
     const newCustomer = await User.create({
                 id_user: newIdCustomer,
                 email: email,
@@ -74,6 +72,7 @@ router.post('/add/account/customer', async function (req, res) {
                 "message": "berhasil register",
     });
 });
+//LOGIN
 router.get('/login', async function (req, res) {
     let { email, password } = req.body;
     const existUser = await User.findAll({
@@ -91,32 +90,35 @@ router.get('/login', async function (req, res) {
         passwordUser.forEach(element => {
             tempPassword = element.password;
         });
-            let passwordHash = tempPassword;
-            if(bcrypt.compareSync(password,passwordHash)){
-                 return res.status(201).send({
-                "message": "Login berhasil",
+        let passwordHash = tempPassword;
+        if (bcrypt.compareSync(password, passwordHash)) {
+            const tempDataUser = await User.findOne({
+                where: {
+                    email : email
+                }
             });
-            }
+            let token = jwt.sign({
+            id_user: tempDataUser.id_user,
+            email: email,
+            firstName: tempDataUser.firstName,
+            lastName: tempDataUser.lastName,
+            birthdate: tempDataUser.birthdate,
+            address: tempDataUser.address,
+            city: tempDataUser.city,
+            province: tempDataUser.province,
+            phone: tempDataUser.phone
+            }, JWT_KEY, { expiresIn: '1800s' });
+        return res.status(200).send({
+        'message': 'Successfully logged in ' + email,
+        token: token
+        });
+        }
             else {
                 return res.status(400).send({
                 "message": "Password salah, login gagal",
                 });
             }
-       
     }
-    // let token = jwt.sign({
-    //     username: username,
-    //     id_account: tempDataUsers.id_account,
-    //     nama: tempDataUsers.nama,
-    //     tipe_akun: tempDataUsers.tipe_akun
-    //     }, JWT_KEY);
-    //     return res.status(200).send({
-    //     'message': 'Successfully logged in ' + username,
-    //     id_account: dataUsers.id_account,
-    //     username: username,
-    //     tipe_user: dataUsers.tipe_akun,
-    //     token: token
-    // });
     else {
         return res.status(404).send({
                 "message": "Data tidak valid, login gagal",
@@ -124,6 +126,71 @@ router.get('/login', async function (req, res) {
     }
 });
 router.post('/add/account/staff', async function (req, res) {
-
+   
+    let { email, password, firstName, lastName, birthdate, address, city, province, phone } = req.body;
+    
+    let token = req.header('x-auth-token');
+    let userdata = jwt.verify(token, JWT_KEY);
+    console.log(userdata.id_user);
+    if (!req.header('x-auth-token')) {
+        return res.status(400).send('Unauthorized')
+    }
+    const schema = Joi.object({
+        email: Joi.string().external(checkEmail).email({ minDomainSegments: 2, tlds: { allow: ['com'] } }).required(),
+        password: Joi.string().required(),
+        firstName: Joi.string().required(),
+        lastName: Joi.string().required(),
+        birthdate: Joi.date().required(),
+        address: Joi.string().required(),
+        city: Joi.string().required(),
+        province: Joi.string().required(),
+        phone: Joi.string().required()
+        });
+        try {
+             await schema.validateAsync(req.body)
+        }     catch (error) {
+             return res.status(400).send(error.toString())
+        }
+    try {
+    if (userdata.id_user == "ADMIN") {
+       let newIdPrefix = "STF"
+            let keyword = `%${newIdPrefix}%`
+            let similarUID = await User.findAll(
+                {
+                    where: {
+                        id_user: {
+                            [Op.like]: keyword
+                        }
+                    }
+                }
+            );
+    let newIdStaff = newIdPrefix + (similarUID.length + 1).toString().padStart(3, '0');
+    const passwordHash = bcrypt.hashSync(password, 10);
+    const newStaff = await User.create({
+                id_user: newIdStaff,
+                email: email,
+                password: passwordHash,
+                firstName: firstName,
+                lastName: lastName,
+                birthdate: birthdate,
+                address: address,
+                city: city,
+                province: province,
+                phone: phone,
+                status: 1
+    });
+        return res.status(201).send({
+                "message": "berhasil register",
+    });
+    }
+    else {
+        return res.status(400).send({
+                message: 'Bukan role Admin, tidak dapat menggunakan fitur'
+            });
+    }
+    } catch (error) {
+        return res.status(400).send('Invalid JWT Key');
+    }
+    
 });
 module.exports = router;
